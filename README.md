92a81ffd08ba4fcc8c7503a57c36a03ee97f1404
Joyce Guo <jog225@lehigh.edu>
2024-06-25 21:47:41 -0400

    adding templates

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
   - Windows: set FLASK_APP = flaskBlog.py
   - Mac: export FLASK_APP = flaskBlog.py
   - flask run
+
+- To open debugger:
+  - Windows: set FLASK_DEBUG = 1
+  - Mac: export FLASK_DEBUG = 1
--- flaskBlog.py
+++ flaskBlog.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
+def home():
+  return render_template('home.html')
 
-def hello():
-  return "Hello World!"
+@app.route("/about")
+def about():
+  return "<h1>ABOUT!</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
