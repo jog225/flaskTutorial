2ae3a796071f5a721f708bb78a006c9d187e43a6
Joyce Guo <jog225@lehigh.edu>
2024-06-25 22:35:51 -0400

    added dummy data

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+  {
+    'author': 'Joyce Guo',
+    'title': 'Blog 1',
+    'content': 'Just started this tutorial',
+    'date_posted': 'June 25, 2024'
+  },
+  {
+    'author': 'Joyce Guo',
+    'title': 'Blog 2',
+    'content': 'Made a bet with Brian to see who finishes this first',
+    'date_posted': 'June 25, 2024'
+  },
+  
+]
+
 @app.route("/home")
 def home():
-  return render_template('home.html')
+  return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
